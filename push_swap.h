a342b22fc9c3c301df3dc8d3ed5750be08d3ed6c
Konrad Girczyc <kgirczyc@c3r12s2.42warsaw.pl>
2026-08-14 16:14:08 +0200

    main

--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,6 @@
+#ifndef "PUSH_SWAP_H"
+# define "PUSH_SWAP_H"
+
 #include <stdlib.h>
 
 typedef struct s_list
@@ -14,6 +17,21 @@ typedef struct s_stack
 	t_list	*last_node;
 	int		size;
 }	t_stack;
+
+typedef struct s_operations
+{
+	int use_pa = 0;
+	int use_pb = 0;
+	int use_sa = 0;
+	int use_sb = 0;
+	int use_ss = 0;
+	int use_ra = 0;
+	int use_rb = 0;
+	int use_rr = 0;
+	int use_rra = 0;
+	int use_rrb = 0;
+	int use_rrr = 0;
+} t_operations;
 
 void	sa(t_stack *stack, int *counter);
 void	sb(t_stack *stack, int *counter);
@@ -35,3 +53,5 @@ void	sort(t_stack *stack_a, t_stack *stack_b);
 void	put_to_stack_b(t_stack *stack_a, t_stack *stack_b, t_list *node_a);
 void	put_to_stack_a(t_stack *stack_a, t_stack *stack_b);
 void medium_sort(t_stack *stack_a, t_stack *stack_b, int *counter);
+
+#endif
